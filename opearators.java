96fa3bc205e2769385a71fc5ede294c877ef7ed9
Manikant Mishra <manimishra2006@gmail.com>
2025-08-13 10:00:47 +0530

    opeartorsbasics

---
+++ opearators.java
@@ -0,0 +1,19 @@
+//OPERATORS IN JAVA
+// int x = 5;
+// int y = 3;
+// System.out.println(x > y); // returns true, because 5 is higher than 3
+
+
+//BASICS
+//1.// ==	Equal to	x == y	
+//2.//!=	Not equal	x != y	
+//3.//>	Greater than	x > y	
+//4.//<	Less than	x < y	
+//5.// >=	Greater than or equal to	x >= y	
+//6.//<=	Less than or equal to	x <= y
+
+//-----------------//
+// && 	Logical and	Returns true if both statements are true	x < 5 &&  x < 10	
+// || 	Logical or	Returns true if one of the statements is true	x < 5 || x < 4	
+// !	Logical not	Reverse the result, returns false if the result is true	!(x < 5 && x < 10)
+
